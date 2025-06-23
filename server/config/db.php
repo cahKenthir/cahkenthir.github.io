8fdf197f0b090430a146a0c366335b6467911c8e
cahKenthir <178104487+cahKenthir@users.noreply.github.com>
2025-06-23 20:24:32 +0700

    Create db.php

---
+++ server/config/db.php
@@ -0,0 +1,12 @@
+<?php
+$db_host = 'localhost';
+$db_name = 'telkomsel_sales';
+$db_user = 'root';
+$db_pass = '';
+
+// In production, use environment variables instead
+// $db_host = getenv('DB_HOST');
+// $db_name = getenv('DB_NAME');
+// $db_user = getenv('DB_USER');
+// $db_pass = getenv('DB_PASS');
+?>
